5447403d7b44f99ed63947a0e79803b23d4cb1da
srm <srm@bouh.org>
2026-04-13 21:29:01 +0200

    chore: new currency ok

--- internal/reporter.go
+++ internal/reporter.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 
 	"github.com/syrm/maille/internal/domain"
-	"github.com/syrm/maille/internal/domain/api"
 )
 
 const recentTransactionsLimit = 6
@@ -13,7 +12,6 @@ const recentTransactionsLimit = 6
 type transactionStatsProvider interface {
 	GetCheckingBalance(context.Context) (int64, error)
 	GetRecentTransactions(context.Context, uint) ([]domain.RecentTransaction, error)
-	GetRecentTransactionsAPI(context.Context, uint) ([]api.Transaction, error)
 	BalanceSummary(ctx context.Context) (domain.BalanceSummary, error)
 }
 
@@ -35,31 +33,6 @@ func (r Reporter) BalanceSummary(ctx context.Context) domain.BalanceSummary {
 
 func (r Reporter) RecentTransactions(ctx context.Context) []domain.RecentTransaction {
 	transactions, err := r.TransactionStatsProvider.GetRecentTransactions(ctx, recentTransactionsLimit)
-
-	if err != nil {
-		r.Logger.ErrorContext(ctx, "failed to get recent transaction", slog.Any("error", err))
-		return nil
-	}
-
-	return transactions
-}
-
-func (r Reporter) BalanceSummaryAPI(ctx context.Context) api.BalanceSummary {
-	checkingBalance, err := r.TransactionStatsProvider.GetCheckingBalance(ctx)
-
-	if err != nil {
-		r.Logger.ErrorContext(ctx, "failed to get balance summary", slog.Any("error", err))
-		return api.BalanceSummary{}
-	}
-
-	return api.BalanceSummary{
-		TotalBalance:    checkingBalance,
-		CheckingBalance: checkingBalance,
-	}
-}
-
-func (r Reporter) RecentTransactionsAPI(ctx context.Context) []api.Transaction {
-	transactions, err := r.TransactionStatsProvider.GetRecentTransactionsAPI(ctx, recentTransactionsLimit)
 
 	if err != nil {
 		r.Logger.ErrorContext(ctx, "failed to get recent transaction", slog.Any("error", err))
